61521105f249270736543f730e89cb4a57b254c4
Himanshutomar12 <tomarhimanshu52@gmail.com>
2024-08-25 14:06:13 +0530

    Final Commit

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -6,7 +6,7 @@ export default function Home() {
     <div>
       {/* <Navbar /> */}
       <div className="flex flex-col items-center justify-center h-screen">
-        <h1 className="text-4xl font-bold mb-6">Welcome to the Tomar's Store</h1>
+        <h1 className="text-4xl font-bold mb-6">Welcome to the Tomar&apos;s Store</h1>
         <div className="space-x-4">
           <Link href="/auth/login">
             <span className="px-4 py-2 bg-blue-500 text-white rounded">Login</span>
